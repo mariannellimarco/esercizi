56836bbf8aeff2211008f33fbcf951102c377b52
mariannellimarco <marco.mariannelli@studenti.unipg.it>
2019-11-17 20:51:53 +0100

    eserciziocifrario

---
+++ string_cripto.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+int main(void){
+
+int i,n,k,metodo; //chiave lunghezza e metodo
+char stringa[256];
+char stringa2[256];
+char alfabeto_mescolato[26];
+printf("\nParola da criptare\n");
+scanf("%s", stringa);
+
+printf("\nQuale metodo vuoi utilizzare?\n");
+printf("\n1)Il cifrario di Cesare\n");
+printf("2)Il cifrario a sostituzione\n");
+
+scanf("%d",&metodo);
+
+switch (metodo) {
+  case 1:
+  printf("\nHai scelto il cifrario di Cesare\n");
+  break;
+  case 2:
+  printf("\nHai scelto il cifrario a sostituzione\n");
+  break;
+}
+
+if (metodo==1) {
+  printf("\nDigita un numero da 1 a 25\n");
+  scanf("%d", &k);
+  printf("cyphertext : ");
+
+  for (i=0; i<strlen(stringa); i++) {
+
+  if (stringa[i]>='a' && stringa[i]<='z') {
+
+      stringa2[i]=97+(((stringa[i]%97)+k)%26);
+  }
+  else
+      stringa2[i]='a'+k-('z'-stringa[i])-1;
+  }
+  printf("%s\n",stringa2);
+}
+
+if(metodo==2){
+
+printf("\nAlfabeto mescolato : ");
+scanf("%s", alfabeto_mescolato);
+k=strlen(alfabeto_mescolato);
+
+//if (isalpha(alfabeto_mescolato) && islower(alfabeto_mescolato)) {
+//for (k = 0; k < strlen(alfabeto_mescolato); k++) {
+for (i = 0; i < strlen(stringa); i++) {
+  alfabeto_mescolato[i]=alfabeto_mescolato[i%k]-'a';
+  stringa[i]=stringa[i]+alfabeto_mescolato[i];
+  printf("%d\n", stringa[i]);
+}
+
+
+
+}
+
+return 0;
+}
